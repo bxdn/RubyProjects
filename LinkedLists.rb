19736202f596da60c6b701e7b1303d0f2b9f13cc
bxdn <sean.smolinski@gmail.com>
2018-09-15 11:41:02 -0400

    Users can't break this now; we are safe.

--- LinkedLists.rb
+++ LinkedLists.rb
@@ -1,26 +1,36 @@
 class LinkedList
-    attr_reader :head, :tail
-    def initialize
-        @tail = Node.new
-        @head = Node.new(@tail)
+    class Node
+        attr_accessor :value, :next_node
+        def initialize(next_node=nil,value=nil)
+            @next_node = next_node
+            @value = value
+        end
     end
-    def append
-        priorTail = @tail
-        @tail = Node.new
-        priorTail.next_node = @tail
+    def initialize(head_v=nil,tail_v=nil)
+        @tail = Node.new(nil,tail_v)
+        @head = Node.new(@tail,head_v)
     end
-    def prepend
-        old_first = @head
-        node = Node.new(nil,old_first)
-        @head = node
+    def append(value=nil)
+        @tail.next_node = Node.new(nil,value)
+        @tail = @tail.next_node   
+    end
+    def prepend(value=nil)
+        @head = Node.new(@head,value)
     end
     def size(node=@head)
         return node==@tail ? 1 : 1 + size(node.next_node)
     end
-    def at(idx,node=@head)
-        return idx == 0 ? node : at(idx-1,node.next_node)
+    def head
+        return @head.value
+    end
+    def tail
+        return @tail.value
+    end
+    def at(idx)
+        return node_at(idx).value
     end
     def pop(node=@head)
+        size_err
         (@tail = node; @tail.next_node = nil; return) if node.next_node == @tail
         return pop(node.next_node)
     end
@@ -37,21 +47,32 @@ class LinkedList
     def to_s(node=@head)
         return node == @tail ? "(#{node.value}) -> nil" : "(#{node.value}) -> #{to_s(node.next_node)}"
     end
-    def insert_at(idx)
-        node = Node.new(at(idx))
+    def insert_at(idx,value=nil)
+        idx_err(idx)
+        node = Node.new(node_at(idx),value)
         (@head = node; return) if idx == 0
-        at(idx-1).next_node = node
+        node_at(idx-1).next_node = node
     end
     def remove_at(idx)
-        (@head = at(1); return) if idx == 0
-        (@tail = at(idx-1); @tail.next_node = nil; return) if at(idx) == @tail
-        at(idx-1).next_node = at(idx).next_node
+        idx_err(idx);size_err
+        (@head = node_at(1); return) if idx == 0
+        node = node_at(idx)
+        prevNode = node_at(idx-1)
+        (@tail = prevNode; @tail.next_node = nil; return) if node == @tail
+        prevNode.next_node = node.next_node
     end
-end
-class Node
-    attr_accessor :value, :next_node
-    def initialize(next_node=nil)
-        @value = nil
-        @next_node = next_node
+    def set_value(idx,value)
+        node_at(idx).value = value
+    end
+    private
+    def node_at(idx,node=@head)
+        idx_err(idx)
+        return idx == 0 ? node : node_at(idx-1,node.next_node)
+    end
+    def idx_err(idx)
+        (puts "ERROR: Cannot index #{idx}; index out of bounds"; exit!) if (idx >= size or idx < 0)
+    end
+    def size_err
+        (puts "ERROR: Minimum size of LinkedList object is 2"; exit!) if size<3
     end
 end
